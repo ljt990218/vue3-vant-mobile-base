704a2485f8ad42666e073380ca0f57ab986f1c94
ljt990218 <2511590093@qq.com>
2024-06-07 12:17:43 +0800

    Initial commit

---
+++ src/components/Container.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps({
+  paddingT: {
+    type: Number,
+    default: 46,
+  },
+  paddingX: {
+    type: Number,
+    default: 16,
+  },
+})
+</script>
+
+<template>
+  <main :style="`padding-top: ${paddingT}px; padding-left: ${paddingX}px; padding-right: ${paddingX}px`">
+    <slot />
+  </main>
+</template>
---
+++ src/pages/profile/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+definePage({
+  name: 'profile',
+  meta: {
+    level: 1,
+  },
+})
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <Container padding-t="0">
+    <div mx-auto mb-60 pt-15 text-center text-16 text-dark dark:text-white>
+      {{ t('profile.placeholder') }}
+    </div>
+  </Container>
+</template>
